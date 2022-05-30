b0797b80acc839900a8e09b62f02b49d77ff2b16
John Alexander Sanchez Rengifo <jasanchezrj@celsia.com>
2022-05-29 19:51:57 -0500

    Update report_comm.php

--- application/views/reports/report_comm.php
+++ application/views/reports/report_comm.php
@@ -98,7 +98,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                         
                         <div class="x_panel">
                             <div class="x_title">
-                                <h2>Ingresos General</h2>
+                                <h2>Reporte de Comisiones</h2>
                                 <ul class="nav navbar-right panel_toolbox">
                                     <li><a class="collapse-link"><i class="fa fa-chevron-up"></i></a>
                                     </li>
