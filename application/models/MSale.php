a89b972c42877c37853104ab06fae7279a763105
John Alexander Sanchez Rengifo <jasanchezrj@celsia.com>
2022-05-30 00:51:42 -0500

    Ajuste add producto venta

--- application/models/MSale.php
+++ application/models/MSale.php
@@ -317,14 +317,22 @@ class MSale extends CI_Model {
 
                     if ($this->config->item('mod_commision') == 1) { //Modulo Comisiones Habilitado
                         
-                        /*Producto de la venta*/
-                        $query = $this->db->query("SELECT
-                                                p.idProducto
-                                                FROM productos p
-                                                LEFT JOIN config_venta_detalle cv ON (cv.idProducto = p.idProducto AND idCliente = '".$this->session->userdata('sclient')."')
-                                                WHERE p.idProducto = ".$item."
-                                                AND ((p.valorProducto - cv.valorDescProd) = '".$value."') OR (p.valorProducto = '".$value."')");
-
+                        if ($this->session->userdata('sclient') == '999999'){ //Usuario generico
+                            /*Producto de la venta*/
+                            $query = $this->db->query("SELECT
+                                                    p.idProducto
+                                                    FROM productos p
+                                                    LEFT JOIN config_venta_detalle cv ON (cv.idProducto = p.idProducto AND idCliente = '".$this->session->userdata('sclient')."')
+                                                    WHERE p.idProducto = ".$item."
+                                                    AND ((p.valorProducto - cv.valorDescProd) = '".$value."') OR (p.valorProducto = '".$value."')");
+                        } else {
+                            $query = $this->db->query("SELECT
+                                                    p.idProducto
+                                                    FROM productos p
+                                                    LEFT JOIN config_venta_detalle cv ON (cv.idProducto = p.idProducto AND idCliente = '".$this->session->userdata('sclient')."')
+                                                    WHERE p.idProducto = ".$item."
+                                                    AND ((p.valorProducto - cv.valorDescProd) = '".$value."')");
+                        }      
                     }
 
                 }
