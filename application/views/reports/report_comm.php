759a5e1436ae0263c551f22a6869731fe8d3f3f1
John Alexander Sanchez Rengifo <jasanchezrj@celsia.com>
2022-05-30 14:22:44 -0500

    Update report_comm.php

--- application/views/reports/report_comm.php
+++ application/views/reports/report_comm.php
@@ -243,20 +243,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     
     <!-- ECharts -->
     <!--<script src="<?php //echo base_url().'public/gentelella/vendors/echarts/dist/echarts.min.js'; ?>"></script>-->
-    <script>
-        $(document).ready(function() {
-            var table = $('#datatable-buttons').DataTable( {
-                scrollY:        "300px",
-                scrollX:        true,
-                scrollCollapse: true,
-                paging:         false,
-                columnDefs: [
-                    { width: '20%', targets: 0 }
-                ],
-                fixedColumns: true
-            } );
-        } );
-    </script>    
+      
     
   </body>
 </html>
