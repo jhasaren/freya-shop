247340684b30cf209949698b72f1e6f9c8e070dd
John Alexander Sanchez Rengifo <jasanchezrj@celsia.com>
2022-06-20 15:57:39 -0500

    Ajuste desc wise

--- application/config/config.php
+++ application/config/config.php
@@ -735,21 +735,21 @@ $config['final_inside_comision'] = 0.02;
 $config['final_otros_descuento'] = 0;
 $config['final_otros_comision'] = 0.02;
 /*GIMNASIO_ENTRENADOR*/
-$config['gim_wise_descuento'] = 0;
+$config['gim_wise_descuento'] = 0.0028573;
 $config['gim_wise_comision'] = 0.05;
 $config['gim_inside_descuento'] = 0.25;
 $config['gim_inside_comision'] = 0.02;
 $config['gim_otros_descuento'] = 0;
 $config['gim_otros_comision'] = 0.02;
 /*MAYORISTA*/
-$config['mayor_wise_descuento'] = 0;
+$config['mayor_wise_descuento'] = 0.0041452;
 $config['mayor_wise_comision'] = 0.05;
 $config['mayor_inside_descuento'] = 0.33;
 $config['mayor_inside_comision'] = 0.02;
 $config['mayor_otros_descuento'] = 0;
 $config['mayor_otros_comision'] = 0.02;
 /*SUPERMAYORISTA*/
-$config['supmayor_wise_descuento'] = 0;
+$config['supmayor_wise_descuento'] = 0.00468375;
 $config['supmayor_wise_comision'] = 0.05;
 $config['supmayor_inside_descuento'] = 0.38;
 $config['supmayor_inside_comision'] = 0.02;
