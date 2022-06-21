c695b5c63874be44b8d37a94610607046b59ec30
John Alexander Sanchez Rengifo <jasanchezrj@celsia.com>
2022-06-20 20:48:43 -0500

    Update report_payment.php

--- application/views/reports/report_payment.php
+++ application/views/reports/report_payment.php
@@ -132,7 +132,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <div class="col-md-12 col-sm-12 col-xs-12">
                     <div class="x_panel">
                         <div class="x_title">
-                            <h2>Detalle [Pagados & Anulados]</h2>
+                            <h2>Detalle [Pagados / Anulados / Cuentas x Cobrar]</h2>
                             <br /><br />
                             <B>Venta:</B> valor general de la venta |
                             <B>Liquidado:</B> valor a pagar <br />
