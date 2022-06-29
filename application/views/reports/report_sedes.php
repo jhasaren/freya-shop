3c09de62da67d18eca1025ed06a975ec1d88bd3c
John Alexander Sanchez Rengifo <jasanchezrj@celsia.com>
2022-06-29 12:14:47 -0500

    Update report_sedes.php

--- application/views/reports/report_sedes.php
+++ application/views/reports/report_sedes.php
@@ -72,6 +72,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                                     <a class="btn btn-info" href="<?php echo base_url().'index.php/CReport/module/reportGastos'; ?>"><i class="glyphicon glyphicon-arrow-up"></i> Gastos</a>
                                 </span>
                             </div>
+                            <div class="input-group">
+                                <div></div>
+                                <span class="input-group-btn">
+                                    <a class="btn btn-danger" href="<?php echo base_url().'index.php/CReport/module/reportComm'; ?>"><i class="glyphicon glyphicon-usd"></i> Comisiones</a>
+                                </span>
+                            </div>
                             <?php 
                             if ($this->config->item('mod_commision') == 1){ //Manejo de Comisiones habilitado
                             ?>
