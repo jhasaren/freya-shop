83f3e73c0de78a376397794b741187a8633988d7
John Alexander Sanchez Rengifo <jasanchezrj@celsia.com>
2022-07-05 20:01:32 -0500

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -728,7 +728,7 @@ $config['category_client'] = 1;
  * 1-habilitar, 0-deshabilitar
 |--------------------------------------------------------------------------
 */
-$config['sale_yesterday'] = 1;
+$config['sale_yesterday'] = 0;
 
 
 /*
